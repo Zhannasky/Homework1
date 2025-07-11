332487aa002ded8eb0e88dfe069b79d42bf899e8
Zhannasky <zannuly@gmail.com>
2025-07-11 21:56:15 +0300

    added file

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ x / y = ${quotient}`);
 // Задание 5
 // Возведите 2 в 5-ю степень. Результат запишите в переменную result  и выведите на экран с помощью метода alert().
 let z = 2;
-result = z ** 5;
+let result = z ** 5;
 alert(`${z} в 5-ой степени равно ${result}.`);
 
 // Задание 6
@@ -78,13 +78,10 @@ alert(`Итог: num = ${num}.`);
 // Задание 8
 // Создайте переменную age , присвойте ей значение с помощью метода prompt() «Сколько вам лет?». Результат выведите с помощью alert().
 
-let age = 20;
-
-age = Number(prompt('Сколько вам лет?'));
-if (age == 18 || age == 19 || age == 20 || age == 25 || age == 26 || age == 27 || age == 28 || age == 29 || age == 30 || age == 35 || age == 36 || age == 37 || age == 38 || age == 39 || age == 40 || age == 45 || age == 46 || age == 47 || age == 48 || age == 49 || age == 50 || age == 55 || age == 56 || age == 57 || age == 58 || age == 59 || age == 60 || age == 65 || age == 66 || age == 67 || age == 68 || age == 69 || age == 70 || age == 75 || age == 76 || age == 77 || age == 78 || age == 79 || age == 80 || age == 85 || age == 86 || age == 87 || age == 88 || age == 89 || age == 90 || age == 95 || age == 96 || age == 97 || age == 98 || age == 99 || age == 100 || age == 105 || age == 106 || age == 107 || age == 108 || age == 109 || age == 110) { termin = 'лет' };
-if (age == 21 || age == 31 || age == 41 || age == 51 || age == 61 || age == 71 || age == 81 || age == 91 || age == 101) { termin = 'год' };
-if (age == 22 || age == 23 || age == 24 || age == 32 || age == 33 || age == 34 || age == 42 || age == 43 || age == 44 || age == 52 || age == 53 || age == 54 || age == 62 || age == 63 || age == 64 || age == 72 || age == 73 || age == 74 || age == 82 || age == 83 || age == 84 || age == 92 || age == 93 || age == 94 || age == 102 || age == 103 || age == 104) { termin = 'года' };
-alert(`Мы теперь знаем, что Вам ${age} ${termin}.`);
+let age = Number(prompt('Сколько вам лет'));
+let up = 17;
+let newResult = age + up;
+alert(`Через ${up} лет вам будет ${newResult}`);
 
 // Задание 9
 // Создайте объект user, у которого есть следующие свойства: name, age, isAdmin. 
